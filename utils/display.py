7321b933064e60080c8f1f83a3d8260091424583
Shubham Bindal <bindals610@gmail.com>
2024-08-11 22:24:48 +0530

    small updates - display.py

--- utils/display.py
+++ utils/display.py
@@ -20,41 +20,6 @@ def norm_all(store, n_t, n_s):
             nstore[t,s] = unorm(store[t,s])
     return nstore
 
-'''
-def norm_torch(x_all):
-    # runs unity norm on all timesteps of all samples
-    # input is (n_samples, 3,h,w), the torch image format
-    x = x_all.cpu().numpy()
-    xmax = x.max((2,3))
-    xmin = x.min((2,3))
-    xmax = np.expand_dims(xmax,(2,3)) 
-    xmin = np.expand_dims(xmin,(2,3))
-    nstore = (x - xmin)/(xmax - xmin)
-    return torch.from_numpy(nstore)
-
-def gen_tst_context(n_cfeat):
-    """
-    Generate test context vectors
-    """
-    vec = torch.tensor([
-    [1,0,0,0,0], [0,1,0,0,0], [0,0,1,0,0], [0,0,0,1,0], [0,0,0,0,1],  [0,0,0,0,0],      # human, non-human, food, spell, side-facing
-    [1,0,0,0,0], [0,1,0,0,0], [0,0,1,0,0], [0,0,0,1,0], [0,0,0,0,1],  [0,0,0,0,0],      # human, non-human, food, spell, side-facing
-    [1,0,0,0,0], [0,1,0,0,0], [0,0,1,0,0], [0,0,0,1,0], [0,0,0,0,1],  [0,0,0,0,0],      # human, non-human, food, spell, side-facing
-    [1,0,0,0,0], [0,1,0,0,0], [0,0,1,0,0], [0,0,0,1,0], [0,0,0,0,1],  [0,0,0,0,0],      # human, non-human, food, spell, side-facing
-    [1,0,0,0,0], [0,1,0,0,0], [0,0,1,0,0], [0,0,0,1,0], [0,0,0,0,1],  [0,0,0,0,0],      # human, non-human, food, spell, side-facing
-    [1,0,0,0,0], [0,1,0,0,0], [0,0,1,0,0], [0,0,0,1,0], [0,0,0,0,1],  [0,0,0,0,0]]      # human, non-human, food, spell, side-facing
-    )
-    return len(vec), vec
-
-def plot_grid(x,n_sample,n_rows,save_dir,w):
-    # x:(n_sample, 3, h, w)
-    ncols = n_sample//n_rows
-    grid = make_grid(norm_torch(x), nrow=ncols)  # curiously, nrow is number of columns.. or number of items in the row.
-    save_image(grid, save_dir + f"run_image_w{w}.png")
-    print('saved image at ' + save_dir + f"run_image_w{w}.png")
-    return grid
-'''
-
 def plot_sample(x_gen_store, n_sample, nrows, save_dir, fn,  w, save=False):
     '''
     x_gen_store: Array of generated images.
